fb5f86680d67e3797d60a693ae183ac618777ff6
Grace Han <ghan0004@student.monash.com>
2018-08-30 14:56:40 +1000

    xml parsing

--- convertor/generate.py
+++ convertor/generate.py
@@ -1,5 +1,8 @@
 import export
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import sys
+from xml.dom import minidom
+
 
 # read in tree 
 database = sys.argv[1]
@@ -11,7 +14,7 @@ tree =  [[] for i in range(len(rows))]
 for row in rows:
     tree[row[1] + 1].append(row)
 
-
+# preorder dfs
 def dfs(root):
     if root:
         s = []
@@ -19,13 +22,26 @@ def dfs(root):
         while s:
             cur = s.pop()
             print(cur)
-            if len(tree[cur[0] + 1])>1:
-                s.insert(0, tree[cur[0] + 1][1])
-            if len(tree[cur[0] + 1]) > 0:
-                s.insert(0, tree[cur[0] + 1][0])
+            if len(tree) > cur[0] + 1:
+                if len(tree[cur[0] + 1])>1:
+                    s.insert(0, tree[cur[0] + 1][1])
+                if len(tree[cur[0] + 1]) > 0:
+                    s.insert(0, tree[cur[0] + 1][0])
     
 # print(tree)
 dfs(tree[0][0])
 
-# for level in tree:
-    # print(level)
+top = Element('gentra4cp')
+header = SubElement(top, 'header')
+provide = SubElement(top, 'provide')
+dfs(root)
+
+
+
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
