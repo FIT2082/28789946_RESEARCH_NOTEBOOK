6e88517e45e4bff0366fed668fff8e2e719c58ea
Grace Han <ghan0004@student.monash.com>
2018-08-30 15:02:05 +1000

    xml generation

--- convertor/generate.py
+++ convertor/generate.py
@@ -1,6 +1,8 @@
 import export
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import sys
+from xml.etree import ElementTree
+
 from xml.dom import minidom
 
 
@@ -14,14 +16,19 @@ tree =  [[] for i in range(len(rows))]
 for row in rows:
     tree[row[1] + 1].append(row)
 
+top = Element('gentra4cp')
+header = SubElement(top, 'header')
+provide = SubElement(top, 'provide')
+
 # preorder dfs
-def dfs(root):
+def dfs(root, top):
     if root:
         s = []
         s.insert(0, root)
         while s:
             cur = s.pop()
-            print(cur)
+            choicepoint = SubElement(top, 'choicepoint')
+            choicepoint.text = str(cur)
             if len(tree) > cur[0] + 1:
                 if len(tree[cur[0] + 1])>1:
                     s.insert(0, tree[cur[0] + 1][1])
@@ -29,12 +36,15 @@ def dfs(root):
                     s.insert(0, tree[cur[0] + 1][0])
     
 # print(tree)
-dfs(tree[0][0])
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
 
-top = Element('gentra4cp')
-header = SubElement(top, 'header')
-provide = SubElement(top, 'provide')
-dfs(root)
+dfs(tree[0][0], top)
+print(prettify(top))
 
 
 
