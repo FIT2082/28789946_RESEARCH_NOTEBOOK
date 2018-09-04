95fcc6bc17ac799618c1c65ab7423573b07f00a8
Grace Han <ghan0004@student.monash.com>
2018-09-04 12:41:21 +1000

    deleted deprecated generate

--- convertor/generate.py
+++ convertor/generate.py
@@ -4,74 +4,77 @@ import sys
 from xml.etree import ElementTree
 from xml.dom import minidom
 
-
-# read in tree 
+# read in database
 database = sys.argv[1]
 conn = export.create_connection(database)
 
 with conn:
     rows = export.select_all_nodes(conn)
-tree =  [[] for i in range(len(rows))]
-for row in rows:
-    tree[row[1] + 1].append(row)
 
+# node object
+class Node:
+    def __init__(self, id, parent, data, left=False):
+        self.id = id
+        self.parent = parent
+        self.left = left
+        self.data = data
+        self.rc = None
+        self.lc = None
+
+# construct tree
+tree = []
+for node in rows:
+    tree.append(Node(node[0], node[1], node, left=node[2] == 0))
+
+# set children nodes
+for node in tree:
+    if node.parent >= 0:
+        if node.left:
+            tree[node.parent].lc = node.id
+        else:
+            tree[node.parent].rc = node.id
+
+# left preorder traversal
+def dfs(tree):
+    s = []
+    path = []
+    s.append(tree[0])
+    while len(s) > 0:
+        cur = s.pop()
+        path.append(cur)
+        if cur.rc is not None:
+            s.append(tree[cur.rc])
+        if cur.lc is not None:
+            s.append(tree[cur.lc])
+    return path
+path = dfs(tree)
+
+# xml definitions
 top = Element('gentra4cp')
 header = SubElement(top, 'header')
+date = SubElement(header, 'date')
+creator = SubElement(header, 'creator')
 provide = SubElement(top, 'provide')
 
-# preorder dfs
-def dfs(root, top):
-    cident = 0
-    chrono = 0
-    if root:
-        s = []
-        s.insert(0, root)
-        while s:
-            cur = s.pop()
-            print(cur)
-            nident = cur[0]
-            chrono += 1
-            choicepoint = SubElement(top, 'choice-point', {'chrono' : str(chrono), 'nident':str(nident)})
-            if cur[-2] != 0:
-                new_cons = SubElement(top, 'new-constraint', {'chrono' : str(chrono), 'cident': 'c' + str(cident), 'cinternal':str(cur[-1])})
-            else:
-                solution = SubElement(top, 'solved', {'chrono' : str(chrono), 'cident': 'c' + str(cident)})
-                cident +=1
-            if len(tree) > cur[0] + 1:
-                if len(tree[cur[0] + 1])>0:
-                    # if(cur[-2] != 4 or cur[-2] != 3 or cur[-2] != 1):
-                    s.insert(0, tree[cur[0] + 1][0])
-                    # else:
-                        
-                if len(tree[cur[0] + 1]) > 1:
-                    # if(cur[-2] != 4 or cur[-2] != 3 or cur[-2] != 1):
-                    s.insert(0, tree[cur[0] + 1][1])
-    
-# print(tree)
-def prettify(elem):
+# generate xml document
+chrono = 0
+cident = 0
+depth = 0
+for node in path:
+    choicepoint = SubElement(top, 'choice-point', {'chrono' : str(chrono), 'nident':str(node.id)})
+    # if not solved, create new constraint
+    if node.data[-2] != 0:
+        chrono +=1
+        cident +=1
+        new_cons = SubElement(top, 'new-constraint', {'chrono' : str(chrono), 'cident': 'c' + str(cident), 'cinternal':node.data[-1]})
+    # if solve, report solved state
+    else:
+        chrono +=1
+        solution = SubElement(top, 'solved', {'chrono' : str(chrono), 'cident': 'c' + str(cident)})
 
+# pretty prints XML   
+def prettify(elem):
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
-
-
-
-def dfs2(root):
-    if root:
-        s = []
-        s.insert(0, root)
-        while s:
-            cur = s.pop()
-            print(cur[0])
-            if len(tree) > cur[0] + 1:
-                s.insert(0, tree[cur[0] + 1][0])
-            if len(tree[cur[0] + 1]) > 1:
-                s.insert(0, tree[cur[0] + 1][1])
-
-# dfs(tree[0][0], top)
-dfs2(tree[0][0])
 print(prettify(top))
-# print(top)
-
-
-
